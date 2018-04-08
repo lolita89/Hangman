d2f3c778a0697cf1b0ced6ecd35541104eeb6f73
lolita89 <cika.lolita@hotmail.com>
2018-04-08 19:43:38 +0200

    functions done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var wrongGuesses = 0;
+var rightGuesses = 0;
+
 class App extends Component {
+
+  // ===================================================
+  reset(eventHangman){
+    window.location.reload();
+  }
+  // ===================================================
+
+  onLetterClick(eventHangman){
+    //to get the letter from the button
+    var letter = eventHangman.currentTarget.id;
+
+    // to check the letter
+    if (letter === 'G'){
+      // show message
+      alert('correct letter!');
+      // show the letter to board (react way??)
+      document.getElementById('letter_g').value='G';
+      // update the total number of right guesses
+      rightGuesses = rightGuesses + 1;
+    } else if (letter === 'O'){
+      alert('correct letter!');
+      document.getElementById('letter_o').value='O';
+      rightGuesses = rightGuesses + 1;
+    } else if (letter === 'A'){
+      alert('correct letter!');
+      document.getElementById('letter_a').value='A';
+      rightGuesses = rightGuesses + 1;
+    } else if (letter === 'L'){
+      alert('correct letter!');
+      document.getElementById('letter_l').value='L';
+      rightGuesses = rightGuesses + 1;
+    } else {
+      wrongGuesses = wrongGuesses + 1;
+      alert('wrong  letter!')
+    }
+
+    // if user guess all correct letters
+    if (rightGuesses === 4){
+      alert('Congrats! You won!');
+    }
+
+    // if user have 9 times wrong
+    if (wrongGuesses === 9){
+      alert('GAME OVER!');
+    }
+  }
+  // ===================================================
+
+  // ===================================================
   render() {
     return (
       <div className="App">
@@ -12,43 +64,49 @@ class App extends Component {
         </header>
         <body className="App-game">
         <div class="wrapper">
-        <input type="text" disabled/>
-        <input type="text" disabled/>
-        <input type="text" disabled/>
-        <input type="text" disabled/>
+        <input id="letter_g" type="text" disabled="disabled"/>
+        <input id="letter_o" type="text" disabled="disabled"/>
+        <input id="letter_a" type="text" disabled="disabled"/>
+        <input id="letter_l" type="text" disabled="disabled"/>
         <br/>
         <br/>
-          <button type="button">A</button>
-          <button type="button">B</button>
-          <button type="button">C</button>
-          <button type="button">D</button>
-          <button type="button">E</button>
-          <button type="button">F</button>
-          <button type="button">G</button>
-          <button type="button">H</button>
-          <button type="button">I</button>
-          <button type="button">J</button>
-          <button type="button">K</button>
-          <button type="button">L</button>
-          <button type="button">M</button>
-          <button type="button">N</button>
-          <button type="button">O</button>
-          <button type="button">P</button>
-          <button type="button">Q</button>
-          <button type="button">R</button>
-          <button type="button">S</button>
-          <button type="button">T</button>
-          <button type="button">U</button>
-          <button type="button">V</button>
-          <button type="button">W</button>
-          <button type="button">X</button>
-          <button type="button">Y</button>
-          <button type="button">Z</button>
-</div>
-</body>
+          <button onClick={this.onLetterClick} id="A" type="button">A</button>
+          <button onClick={this.onLetterClick} id="B" type="button">B</button>
+          <button onClick={this.onLetterClick} id="C" type="button">C</button>
+          <button onClick={this.onLetterClick} id="D" type="button">D</button>
+          <button onClick={this.onLetterClick} id="E" type="button">E</button>
+          <button onClick={this.onLetterClick} id="F" type="button">F</button>
+          <button onClick={this.onLetterClick} id="G" type="button">G</button>
+          <button onClick={this.onLetterClick} id="H" type="button">H</button>
+          <button onClick={this.onLetterClick} id="I" type="button">I</button>
+          <button onClick={this.onLetterClick} id="J" type="button">J</button>
+          <button onClick={this.onLetterClick} id="K" type="button">K</button>
+          <button onClick={this.onLetterClick} id="L" type="button">L</button>
+          <button onClick={this.onLetterClick} id="M" type="button">M</button>
+          <button onClick={this.onLetterClick} id="N" type="button">N</button>
+          <button onClick={this.onLetterClick} id="O" type="button">O</button>
+          <button onClick={this.onLetterClick} id="P" type="button">P</button>
+          <button onClick={this.onLetterClick} id="Q" type="button">Q</button>
+          <button onClick={this.onLetterClick} id="R" type="button">R</button>
+          <button onClick={this.onLetterClick} id="S" type="button">S</button>
+          <button onClick={this.onLetterClick} id="T" type="button">T</button>
+          <button onClick={this.onLetterClick} id="U" type="button">U</button>
+          <button onClick={this.onLetterClick} id="V" type="button">V</button>
+          <button onClick={this.onLetterClick} id="W" type="button">W</button>
+          <button onClick={this.onLetterClick} id="X" type="button">X</button>
+          <button onClick={this.onLetterClick} id="Y" type="button">Y</button>
+          <button onClick={this.onLetterClick} id="Z" type="button">Z</button>
+          <br/>
+          <br/>
+          <button onClick={this.reset} id="reset" type="button">RESET</button>
+
+        </div>
+        </body>
       </div>
     );
   }
+  // ===================================================
 }
+
 
 export default App;
